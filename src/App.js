f6bf255647f21e1ea6803d50e7592f9fb1172cd3
joilsonLeal <joilson_rg@hotmail.com>
2020-07-27 20:26:44 -0300

    chore: utilizaçao useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
+
   function handleAdd() {
     setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  // componentDidMount
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if(storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+
+    // componentWillUnmount
+    // return () => {
+    //   document.removeEventListener();
+    // };
+  }, []);
+
+  compon
+
+  // componentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
