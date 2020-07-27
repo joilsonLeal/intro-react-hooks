96f4e9b6ab18f4a31a2a1dd452634ecc936bd736
joilsonLeal <joilson_rg@hotmail.com>
2020-07-27 20:32:31 -0300

    chore: utilizaçao useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [techs, setTechs] = useState([]);
@@ -23,18 +23,19 @@ function App() {
     // };
   }, []);
 
-  compon
-
   // componentDidUpdate
   useEffect(() => {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
+
+  const techSize = useMemo(() => techs.length, [techs]);
 
   return (
     <>
       <ul>
         { techs.map(tech => <li key={tech}>{tech}</li>) }
       </ul>
+      <div><strong>Você tem {techSize} tecnologias</strong></div>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </>
