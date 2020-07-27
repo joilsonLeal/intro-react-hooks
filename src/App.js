d32c376708df437fa56516d9405a91d81ef6e568
joilsonLeal <joilson_rg@hotmail.com>
2020-07-27 19:25:21 -0300

    chore: utilizaçao useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <h1>Hello react hooks</h1>
+    <>
+      <ul>
+        { techs.map(tech => <li key={tech}>{tech}</li>) }
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>
+    </>
   );
 }
 
